9589a7b22cc993feab0f5c430dfb645983de3d0f
smtr42 <simteiva@gmail.com>
2021-04-25 09:52:32 +0200

    Fix typo

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,16 +1,16 @@
 from . import *
 
 DEBUG = False
-SECRET_KEY = env('SECRET_KEY')
-ALLOWED_HOSTS = env('ALLOWED_HOSTS')
+SECRET_KEY = env("SECRET_KEY")
+ALLOWED_HOSTS = env("ALLOWED_HOSTS")
 
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": env('DATABASE_NAME'),
-        "USER": env('DATABASDATABASE_USERE_NAME'),
-        "PASSWORD": env('DATABASE_PASS'),
-        "HOST": env('DATABASE_HOST'),
+        "NAME": env("DATABASE_NAME"),
+        "USER": env("DATABASE_USER"),
+        "PASSWORD": env("DATABASE_PASS"),
+        "HOST": env("DATABASE_HOST"),
         "PORT": "5432",
     }
 }
